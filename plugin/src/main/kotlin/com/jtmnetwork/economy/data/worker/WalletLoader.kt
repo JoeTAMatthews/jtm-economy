4b107a4c88fac2aff45246be3e4255cab74cb7de
Joe Matthews <joetymatthews@gmail.com>
2022-03-04 05:09:10 +0000

    refactor: update worker

--- plugin/src/main/kotlin/com/jtmnetwork/economy/data/worker/WalletLoader.kt
+++ plugin/src/main/kotlin/com/jtmnetwork/economy/data/worker/WalletLoader.kt
@@ -8,8 +8,6 @@ import org.bukkit.entity.Player
 
 class WalletLoader(private val framework: Framework, private val service: WalletService, private val player: Player): Runnable {
 
-    private val logging = framework.getLogging()
-
     override fun run() {
         val wallet: Wallet = if (service.exists(player.uniqueId.toString())) service.get(player.uniqueId.toString())!! else service.insert(Wallet(player.uniqueId.toString(), player.name))!!
         framework.runTask { framework.callEvent(WalletLoadEvent(wallet)) }
