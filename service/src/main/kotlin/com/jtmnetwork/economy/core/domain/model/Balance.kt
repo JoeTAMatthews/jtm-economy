fff0311e8d062cf1e218290d001c6064d281275a
Joe Matthews <joetymatthews@gmail.com>
2022-04-20 19:47:37 +0100

    new: add cors for rest calls on a website.

---
+++ service/src/main/kotlin/com/jtmnetwork/economy/core/domain/model/Balance.kt
@@ -0,0 +1,5 @@
+package com.jtmnetwork.economy.core.domain.model
+
+import java.util.*
+
+data class Balance(val id: UUID, val amount: Double)
---
+++ service/src/main/kotlin/com/jtmnetwork/economy/entrypoint/configuration/CorsGlobalConfiguration.kt
@@ -0,0 +1,18 @@
+package com.jtmnetwork.economy.entrypoint.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@EnableWebFlux
+@Configuration
+open class CorsGlobalConfiguration: WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "OPTIONS")
+            .maxAge(3600)
+    }
+}
